c8e2da625bcda7fdebc3509e8bfe3287c0b637c7
Ned Palacios <npdoesmc@gmail.com>
2020-04-21 21:31:25 +0800

    updated examples

--- run.v
+++ run.v
@@ -1,12 +1,17 @@
-module main
+module jisoni
 
 import net.http
 import os
 
 fn main() {
     // Usage: ./[exec name] <url with json content>
-    resp := http.get(os.args[1])?
-    println(os.args[1])
-    mod := decode(resp.text)
-    println(mod)
+    url := 'https://vpkg-project.github.io/registry/registry.json'
+    resp := http.get(url) or { panic('Error') }
+    mod := decode(resp.text) or { panic('Error') }
+
+    // TODO: Better API for retrieving array values
+    packages := mod.get('packages') as Array
+    pkg := packages.values[0] as Object
+
+    println(pkg.get('name'))
 }

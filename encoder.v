132b4c893dc9d1c2af3160efc6b477990a1374fa
Ned Palacios <npdoesmc@gmail.com>
2020-04-24 18:25:23 +0800

    merge str.v to encoder.v

--- encoder.v
+++ encoder.v
@@ -1,29 +1,98 @@
 module jisoni
-
 import strings
 
-pub fn encode(f Field) string {
-    mut g := strings.new_builder(2000)
+pub fn (x Field) str() string {
+    match x {
+        String { return it.value }
+        Bool { return it.value.str() }
+        Object { return it.str() }
+        Int { return it.value.str() }
+        Array { return it.str() }
+        Null { return 'null' }
+        ArrayValue { return it.str() }
+        Undefined { return 'undefined' }
+        else { return 'Unknown' }
+    }
+}
 
-    match f {
-        Object {
-            obj := f as Object
-            if !obj.key.starts_with('Object_') && obj.key.len > 2 {
-                g.write('"${obj.key}":')
-            }
-            g.write(f.str())
+pub fn (arr Array) str() string {
+    return arr.values.str()
+}
+
+pub fn (x Field) json_str() string {
+    mut out := '"' + x.key() + '": '
+
+    match x {
+        String { out += '"${it.value}"' }
+        Object { out += it.str() }
+        Array { out += ' ${it.values.str()}' }
+        else { out += x.str() }
+    }
+
+    return out
+}
+
+pub fn (obj Object) str() string {
+    mut g := strings.new_builder(20000)
+    g.write('{')
+    obj_keys := obj.fields.keys()
+    for i, k in obj_keys {
+        f := obj.fields[k]
+        str := f.json_str()
+        for line in str.split_into_lines() {
+            g.write('\n    ' + line)
         }
-        Array {
-            arr := f as Array
-            if !arr.key.starts_with('Array_') && arr.key.len > 2 {
-                g.write('"${arr.key}":')
-            }
-            g.write(f.str())
+        if i < obj_keys.len-1 { g.write(', ') }
+    }
+    g.write('\n}')
+    return g.str()
+}
+
+pub fn (x ArrayValue) str() string {
+    match x {
+        string { return '"$it"' }
+        int { return it.str() }
+        f64 { return it.strlong() }
+        Object { return it.str() }
+        Array { return it.values.str() }
+        bool { return it.str() }
+        Null { return 'null' }
+        else { return 'null' }
+    }
+}
+
+pub fn (av []ArrayValue) str() string {
+    mut g := strings.new_builder(20000)
+    g.writeln('[')
+
+    for i, x in av {
+        str := x.str()
+        lines := str.split_into_lines()
+        for li, line in lines {
+            g.write('    ' + line)
+            if li == lines.len-1 && i < av.len-1 { g.write(', ') }
+            g.write('\n')
         }
-        else {
-            g.write(f.json_str())
+        // g.write('\n')
+    }
+
+    g.writeln(']')
+
+    return g.str()
+}
+
+pub fn (xs []Field) str() string {
+    mut g := strings.new_builder(20000)
+    g.write('[')
+
+    for i, x in xs {
+        g.write(x.str())
+        if i < xs.len-1 {
+            g.write(', ')
         }
     }
+
+    g.write(']')
 
     return g.str()
 }

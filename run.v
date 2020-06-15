07dd28e3c5c83d62c93ce46b506cda62f8c5287f
Ned Palacios <npdoesmc@gmail.com>
2020-06-15 13:42:50 +0800

    update run.v

--- run.v
+++ run.v
@@ -2,14 +2,33 @@ module main
 
 import os
 
+// struct Person {
+// mut:
+//     name string
+//     age  int = 20
+//     pets []string
+// }
+
+// fn (mut p Person) from_json(f Field) Person {
+//     obj := f.as_map()
+//     for k, v in obj {
+//         match k {
+//             'name' { p.name = v.as_str() }
+//             'age' { p.age = v.as_int() }
+//             'pets' { p.pets = v.as_arr().map(it.as_str()) }
+//             else {}
+//         }
+//     }
+//     return p
+// }
+
 fn main() {
     // Usage: ./[exec name] <url with json content>
     url := os.args[1]
     resp := os.read_file(url) or { panic('error') }
-    mod := decode(resp) or {
+    raw_decode(resp) or {
         eprintln(err)
         exit(1)
     }
-    println(mod)
     exit(0)
 }
